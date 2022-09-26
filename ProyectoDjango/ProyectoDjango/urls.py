a85ed3fbf1bc335a623ecfeb90126d5f621c9e82
Sebastián <sebastian.lema04@inacapmail.cl>
2022-09-26 15:37:08 -0300

    App2

--- ProyectoDjango/ProyectoDjango/urls.py
+++ ProyectoDjango/ProyectoDjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from secondApp import views as app2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app2',app2.display),
 ]
